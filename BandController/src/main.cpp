9d918545103b2b81096df35b8691282a22c218f3
Lukas Kaiser <lukas@lukas-kaiser.at>
2017-03-22 09:46:10 +0100

    first tft and sd tests

--- BandController/src/main.cpp
+++ BandController/src/main.cpp
@@ -2,6 +2,7 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>     // not used for txt
 #include <Adafruit_ILI9340.h>
+#include <SD.h>
 
 #if defined(__SAM3X8E__)
     #undef __FlashStringHelper::F(string_literal)
@@ -17,20 +18,42 @@
 #define _dc 9
 #define _rst 8
 
+#define SD_CS 48
+
 // Using software SPI is really not suggested, its incredibly slow
 //Adafruit_ILI9340 tft = Adafruit_ILI9340(_cs, _dc, _mosi, _sclk, _rst, _miso);
 // Use hardware SPI
 Adafruit_ILI9340 tft = Adafruit_ILI9340(_cs, _dc, _rst);
 
 void testText();
+void printDirectory(File dir, int numTabs);
+
+File root;
 
 void setup()
 {
   pinMode(13, OUTPUT);
 
+  Serial.begin(9600);
+
+  pinMode(SD_CS, OUTPUT);
+  digitalWrite(SD_CS, HIGH);
+
+
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(SD_CS)) {
+    Serial.println("failed!");
+    return;
+  }
+  Serial.println("OK!");
+
+  root = SD.open("/");
+
   tft.begin();
   tft.setRotation(3);
   testText();
+
+  printDirectory(root, 0);
 }
 
 void loop(void)
@@ -41,13 +64,39 @@ void loop(void)
   delay(1000);
 }
 
+void printDirectory(File dir, int numTabs) {
+  while (true) {
+
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+      Serial.print('\t');
+    }
+    Serial.print(entry.name());
+    tft.println(entry.name());
+
+    if (entry.isDirectory()) {
+      Serial.println("/");
+      printDirectory(entry, numTabs + 1);
+    } else {
+      // files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.println(entry.size(), DEC);
+    }
+    entry.close();
+  }
+}
+
 void testText()
 {
   tft.fillScreen(ILI9340_BLACK);
   tft.setCursor(0, 0);
   tft.setTextColor(ILI9340_WHITE);  tft.setTextSize(1);
   tft.println("Cantus Ex Machina");
-  tft.setTextColor(ILI9340_YELLOW); tft.setTextSize(2);
+  /*tft.setTextColor(ILI9340_YELLOW); tft.setTextSize(2);
   tft.println(1234.56);
   tft.setTextColor(ILI9340_RED);    tft.setTextSize(3);
   tft.println(0xDEADBEEF, HEX);
@@ -64,5 +113,5 @@ void testText()
   tft.println("Or I will rend thee");
   tft.println("in the gobberwarts");
   tft.println("with my blurglecruncheon,");
-  tft.println("see if I don't!");
+  tft.println("see if I don't!");*/
 }
