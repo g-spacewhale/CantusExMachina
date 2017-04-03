6873f9a87f9540a135271c77e7afaa8d9b42380e
Lukas Kaiser <lukas@lukas-kaiser.at>
2017-04-03 13:51:11 +0200

    added debug functionality over serial 9600

--- BandController/src/main.cpp
+++ BandController/src/main.cpp
@@ -9,12 +9,18 @@
 // libraries
 #include <Arduino.h>
 #include <EEPROM.h>
+#include <SPI.h>
 
 // custom classes and additional headers for information
 #include <StateDefines.h>
 #include <Display.h>
 
-//#define _DEBUGGING 1   // defines if debugg messages are sent via serial
+#define _DEBUGING   // defines if debugg messages are sent via serial (-> comment out if no messages shoul be sent)
+#ifdef _DEBUGING
+  #define SEND_DEBUG_MESSAGE(x,y) sendDebugMessage(x,y)
+#else
+ #define SEND_DEBUG_MESSAGE(x,y)
+#endif
 
 #define _FIRMWARE_VERSION 0.01
 
@@ -28,33 +34,35 @@
 #define _encoderPinB 3
 #define _endocerPinClick 18
 
-/*#define _displayPinCS 53
+// defines the 4 Data-Pins of the Display
+#define _displayPinCS 53
 #define _displayPinDC 9
 #define _displayPinRST 8
-#define _displayPinBACKLIGHT 4*/
-
+#define _displayPinBACKLIGHT 4
 
 
 // ------------------- Variable declarations -------------------
 unsigned char _state = _STATE_BOOTUP;       // holds the current state of the statemachine - refere to 'Defining of States'
-//unsigned char _previousState = 0;           // holds the previous state of the statemachine
+unsigned char _previousState = 0;           // holds the previous state of the statemachine
 
-Display _display = Display(35, 9, 8, 4);
-
+Display _display = Display(_displayPinCS, _displayPinDC, _displayPinRST, _displayPinBACKLIGHT);
 
 // forward declarations
-//void sendDebugMessage(unsigned char messageCode, String message);
-//void changeState(unsigned char newState);
+void sendDebugMessage(unsigned char messageCode, String message);
+void changeState(unsigned char newState);
 void bootUpRoutine();
 
 
-// Start of Code
+
 void setup()
 {
     // nothing to do here everything is done in the Statemachine loop
 
     // setup serial for debugg messages
-
+    #ifdef _DEBUGING
+      Serial.begin(9600);
+      Serial.println("Debug Messages activated:");
+    #endif
 }
 
 // Statemachine Loop
@@ -64,21 +72,30 @@ void loop()
   {
     case _STATE_BOOTUP:
       bootUpRoutine();
-      _state = _STATE_HOME;
       break;
     case _STATE_HOME:
       break;
     default:
       //Error message
+      SEND_DEBUG_MESSAGE(0, "ERROR: Statemachine went into default case");
       break;
   }
 }
 
-/*void changeState(unsigned char newState)
+void sendDebugMessage(unsigned char messageCode, String message)
+{
+  Serial.print("DebuggMessage(#");
+  Serial.print(messageCode);
+  Serial.print("):");
+  Serial.println(message);
+}
+
+void changeState(unsigned char newState)
 {
   _previousState = _state;
   _state = newState;
-}*/
+  SEND_DEBUG_MESSAGE(0, "Changed State");
+}
 
 void bootUpRoutine()
 {
@@ -88,4 +105,11 @@ void bootUpRoutine()
   // Init SD-Card
   // Load
   // Load infos from SD card
+
+
+
+
+
+  // When all done:
+  changeState(_STATE_HOME);
 }
